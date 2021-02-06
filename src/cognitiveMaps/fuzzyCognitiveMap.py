Add rf and svm classifiers. Fix bug in derivatives

src/cognitiveMaps/fuzzyCognitiveMap.py
@@ -46,4 +46,4 @@ class FuzzyCognitiveMap(BaseCognitiveMap):
             result = FuzzyCognitiveMap.f(self.weights.dot(input_in_time[i]))
             result -= expected_output[i]
             error += result.dot(np.transpose(result))
-        return error
+        return error/len(input_in_time)

src/transformingData/cmeans.py
@@ -27,9 +27,10 @@ def transform(xses_series, centers):
     for xs in xses_series:
         squashed_xses.extend(xs)
     squashed_xses = np.asarray(squashed_xses).transpose()
+    centers=np.asarray(centers)
     result = cmeans_predict(
         test_data=squashed_xses,
-        cntr_trained=np.asarray(centers),
+        cntr_trained=centers,
         m=2.0,
         error=0.001,
         maxiter=1000